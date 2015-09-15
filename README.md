80be53941b2aca5773e40ac4a3059508c8336136
Oda <oda@member.fsf.org>
2015-09-15 12:04:01 -0300

    service focused

--- README.md
+++ README.md
@@ -2,13 +2,12 @@
 The Local Forms module for DrupalGap.
 
 # Usage
-In your `settings.js` file, specify what form id(s) will have be enabled for use
-with the Local Forms module. For example, this enables the usage of Local Forms
-on Webforms:
+In your `settings.js` file, specify what services  will have be enabled for use
+with the Local Forms module. For example, this enables the usage of webform_service
+submission.create:
 
 ```
-drupalgap.settings.local_forms = {
-  forms: ['webform_form']
-};
+drupalgap.settings.local_forms['submission']['create'] = {};
+
 ```
 
--- local_forms.js
+++ local_forms.js
@@ -2,131 +2,46 @@
  * HOOKS
  */
 
-/**
- * Implements hook_form_alter().
- */
-function local_forms_form_alter(form, form_state, form_id) {
-  try {
-    
-    //dpm(form_id);
-    
-    // Skip all forms that aren't set for local storage.
-    if ($.inArray(form_id, local_forms_get_forms()) == -1) { return; }
-    
-    /*dpm('local_forms_form_alter');
-    console.log(form);
-    console.log(form_state);*/
-    
-    // Set aside the the form's default submit handler inside the form, then
-    // replace the form's default submit handler with ours. Then since we set
-    // aside the original submission handler in the form, we can get it back
-    // from local storage during submission, which will allow us to queue it
-    // for later submission.
-    form.local_forms = { };
-    $.each(form.submit, function(index, submit) {
-        
-        // Skip non default submission handlers.
-        if (submit != form_id + '_submit') { return; }
-        
-        form.local_forms.submit = submit;
-        form.submit[index] = 'local_forms_submit';
-
-    });
-
-    // If we have any values in local storage, place them back into the form.
-    var values = variable_get(local_forms_storage_id(form), null);
-    if (values) {
-      values = JSON.parse(values);
-      $.each(form.elements, function(name, element) {
-          if (typeof values[name] === 'undefined') { return; }
-          form.elements[name].value = values[name];
-      });
-    }
-
-  }
-  catch (error) { console.log('local_forms_form_alter - ' + error); }
-}
 
 /**
- * Postprocess a service call.
- * @param {Object} options
- * @param {Object} result
+ * Implements hook_install().
  */
-function local_forms_services_postprocess(options, result) {
+// function local_forms_install() {
+//   document.addEventListener("offline", _onOffline, false);
+//   document.addEventListener("online", _onOnline, false);
+// }
+// function _onOffline() {
+// //   drupalgap.online = false;
+// }
+// function _onOnline() {
+// //   drupalgap.online = true;
+// //   drupalgap_alert('on');
+// }
+function local_forms_services_postprocess(options, request) {
+  // we start to resend submissions when some request was successful
   try {
-    
-    // After a system connect call, we'll assume the user has a connection. At
-    // this time submit any forms that were previously queued up.
-    if (options.service == 'system' && options.resource == 'connect') {
-      var queue = local_forms_get_submission_queue();
-      for (var i = 0; i < queue.length; i++) {
-        var fn = window[queue[i].submit];
-        fn(queue[i].form, queue[i].form_state);
-      }
-      local_forms_clear_submission_queue();
-    }
-    else {
-      
-      // All other service resources...
-      
-      // Support the Webform module.
-      if (module_exists('webform') && options.service == 'webform') {
-  
-        if (options.resource == 'submissions') {
-          
-          //dpm('local forms webform!');
-          //console.log(result);
-          
-        }
-  
-      }  
-      
-    }
-
+    // the last post was successful, send another
+    var submission = local_forms_dequeue_submission();
+    if(typeof submission !== 'undefined')
+      Drupal.services.call(submission);
   }
   catch (error) { console.log('local_forms_services_postprocess - ' + error); }
 }
 
-/**
- * FORMS
- */
-
-/**
- *
- */
-function local_forms_submit(form, form_state) {
+function local_forms_services_postprocess_error(submission, request) {
   try {
-    // Queue the submission.
-    local_forms_queue_submission(form, form_state);
-    // Place the form state values in local storage.
-    variable_set(local_forms_storage_id(form), JSON.stringify(form_state.values));
+    if (typeof drupalgap.settings.local_forms === 'undefined' ||
+        typeof drupalgap.settings.local_forms[submission.service] === 'undefined'||
+        typeof drupalgap.settings.local_forms[submission.service][submission.resource] === 'undefined')
+      return;
+    local_forms_queue_submission(submission);
   }
-  catch (error) { console.log('local_forms_submit - ' + error); }
+  catch (error) { console.log('local_forms_services_postprocess_error - ' + error); }
 }
 
 /**
  * HELPERS
  */
-
-/**
- *
- */
-function local_forms_get_forms() {
-  try {
-    return drupalgap.settings.local_forms.forms;
-  }
-  catch (error) { console.log('local_forms_get_forms - ' + error); }
-}
-
-/**
- *
- */
-function local_forms_storage_id(form) {
-  try {
-    return 'local_forms_' + form.id;
-  }
-  catch (error) { console.log('local_forms_storage_id - ' + error); }
-}
 
 /**
  *
@@ -143,26 +58,34 @@ function local_forms_get_submission_queue() {
 /**
  *
  */
-function local_forms_clear_submission_queue() {
+function local_forms_set_submission_queue(queue) {
   try {
-    variable_set('local_forms_submission_queue', []);
+    variable_set('local_forms_submission_queue', queue);
   }
-  catch (error) { console.log('local_forms_clear_submission_queue - ' + error); }
+  catch (error) { console.log('local_forms_set_submission_queue - ' + error); }
 }
 
 /**
  *
  */
-function local_forms_queue_submission(form, form_state) {
+function local_forms_queue_submission(submission) {
   try {
     var queue = local_forms_get_submission_queue();
-    queue.push({
-        submit: form.local_forms.submit,
-        form: form,
-        form_state: form_state
-    });
-    variable_set('local_forms_submission_queue', queue);
+    queue.push(submission);
+    local_forms_set_submission_queue(queue);
   }
   catch (error) { console.log('local_forms_queue_submission - ' + error); }
 }
 
+/**
+ * Returns the oldest submission if any, undefined otherwise
+ */
+function local_forms_dequeue_submission() {
+  try {
+    var queue = local_forms_get_submission_queue();
+    var submission = queue.shift();
+    local_forms_set_submission_queue(queue);
+    return submission;
+  }
+  catch (error) { console.log('local_forms_dequeue_submission - ' + error); }
+}
